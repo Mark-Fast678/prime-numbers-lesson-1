77ef1f6fbcc7bde36bc1ae4058967a2dbda78e90
Mark-Fast678 <161099090+Mark-Fast678@users.noreply.github.com>
2024-03-01 19:06:58 -0800

    Update main.py to include Lesson 2

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ Lesson goals:
 4) Increase difficulty of the program:
    this time find all prime numbers in the range from 1 to 150 and save them in the list
    discuss list data structure with student
-5) Bonus points: sum up all numbers containing 9 as a digit. Use list comprehension
+5) sum up all numbers containing 9 as a digit
+6) This time use list comprehension to sum up all numbers containing 9 as a digit.
 
 '''
 primes = []
@@ -31,16 +32,35 @@ def is_prime(num):
 # assignment #2
 # find all prime numbers in the range from 1 to 150 and save them in the list
 
-for i in range (1, 150):
+for i in range (1, 100):
     # if i is a prime number, add it to list named primes
     if is_prime(i) == True:
         primes.append(i)
 
 print(primes)
 
-# Next time continue bonus points challenge to learn about list comprehension
-def has_numbers(input_string):
-    return any(char.isdigit() for char in input_string)
+# Lesson 2. Start with no list comprehension
+def contains_digit(input_string, digit):
+    for ch in input_string:
+        if ch == digit:
+            return True
 
+# Lesson 2 Use list comprehension
+def contains_digit_list_comprehension(input_string, digit):
+    return any(ch == digit for ch in input_string)
+
+# not using list comprehension
+sum = 0
 for p in primes:
-    if (str(p))
+    if (contains_digit(str(p), "9")):
+        sum = sum + p
+print(sum)
+
+# using list comprehension:
+# not using list comprehension
+sum = 0
+for p in primes:
+    if (contains_digit_list_comprehension(str(p), "9")):
+        sum = sum + p
+
+print(sum)
